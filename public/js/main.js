7305e984e8cf405d4d42bfefb2c6c7a3a0a6f8e7
Mihai Mijea <mmihai@suitsupply.com>
2017-12-08 20:50:22 +0100

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+function loadImage(url) {
+    return new Promise(resolve => {
+        const image = new Image();
+        image.addEventListener('load', () => {
+            resolve(image);
+        });
+        image.src = url;
+    });
+}
+
+class SpriteSheet {
+    constructor(image, width, height){
+        this.image = image;
+        this.width = width;
+        this.height = height;
+    }
+
+    define(name, x, y) {
+        const buffer = [];
+    }
+}
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+loadImage('images/tile-set.png')
+    .then(image => {
+        const sprites = new SpriteSheet(image);
+        sprites.define('ground', 0, 0);
+        sprites.draw('ground', context, 45, 62);
+        context.drawImage(image,
+            0, 0, 16, 16,
+            0, 0, 16, 16);
+    });
